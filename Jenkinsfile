23e8df7fb7915e935159de9e7ca849e35760639b
srj <sreejithcs273@gmail.com>
2025-09-08 10:49:38 +0530

    demo

---
+++ Jenkinsfile
@@ -0,0 +1,19 @@
+pipeline{
+    agent any
+    stages {
+        stage ('Configure Nginx'){
+            steps {
+                sh 'echo "Step 1"'
+                git branch: 'prod-django', url: 'https://github.com/Rocinate-droid/Furniture_Website.git'
+            }
+        }
+        stage ('Run Django') {
+            steps {
+                cd Furniture_Website
+                python3 -m venv venv
+                source venv/bin/activate
+                python3 -m manage.py runserver
+            }
+        }
+    }
+}
--- furniture/furniture/settings.py
+++ furniture/furniture/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-3%w@xqpj%*r7gh*w+6+)+07l0#eaz2$4v5v33&n6j035xmj0=v"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '34.237.146.151'
+]
 
 
 # Application definition
