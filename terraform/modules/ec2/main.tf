d43ea338b4ada112b8d06122de0f5d23f63635f4
srj <sreejithcs273@gmail.com>
2025-09-08 11:52:52 +0530

    infra changes

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -17,6 +17,14 @@ resource "aws_instance" "django-host" {
             apt update -y
             apt upgrade -y
             apt install -y python3-pip python3-venv
+            apt install openjdk-21-jdk -y
+            wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+            https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+            echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+            https://pkg.jenkins.io/debian-stable binary/ | tee \
+            /etc/apt/sources.list.d/jenkins.list > /dev/null
+            apt update
+            apt install jenkins -y
             EOF
 
 }
