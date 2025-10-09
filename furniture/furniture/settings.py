b871030d15dea6bf83c1e3350a2e77aff63d01fa
srj <sreejithcs273@gmail.com>
2025-10-09 18:49:43 +0530

    fault check

--- furniture/furniture/settings.py
+++ furniture/furniture/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '52.6.185.146'
