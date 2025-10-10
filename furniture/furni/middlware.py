01b8e3fc6df58430d957154e90939f50b4ee0163
srj <sreejithcs273@gmail.com>
2025-10-10 13:20:16 +0530

    cart middleware

---
+++ furniture/furni/middlware.py
@@ -0,0 +1,11 @@
+import uuid
+
+class AnonymousCartMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if not request.user.is_authenticated:
+            if not request.session.get('user_id'):
+                request.session['user_id'] = str(uuid.uuid4())
+        return self.get_response(request)
--- furniture/furniture/settings.py
+++ furniture/furniture/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "furni.middleware.AnonymousCartMiddleware",
 ]
 
 ROOT_URLCONF = "furniture.urls"
