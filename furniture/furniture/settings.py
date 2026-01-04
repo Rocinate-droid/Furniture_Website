c470743173330e7fe528e50c22172b0415f0145e
srj <sreejithcs273@gmail.com>
2026-01-04 20:47:32 +0530

    settings change

--- furniture/furniture/settings.py
+++ furniture/furniture/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = "/var/www/myproject/static/"
 
 RAZOR_KEY_ID = env('RAZOR_KEY_ID')
 RAZOR_KEY_SECRET = env('RAZOR_KEY_SECRET')
@@ -148,7 +148,7 @@ RAZOR_KEY_SECRET = env('RAZOR_KEY_SECRET')
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = '/media/' 
-MEDIA_ROOT = BASE_DIR / 'media'  
+MEDIA_ROOT = "/var/www/myproject/media/" 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
