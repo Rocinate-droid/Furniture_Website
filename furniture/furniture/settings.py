6c9c7e5c88890ffd0c5055ecb0a0887d7dd461bc
srj <sreejithcs273@gmail.com>
2026-01-05 17:57:09 +0530

    settings revert

--- furniture/furniture/settings.py
+++ furniture/furniture/settings.py
@@ -11,7 +11,7 @@ import environ
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
-
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
