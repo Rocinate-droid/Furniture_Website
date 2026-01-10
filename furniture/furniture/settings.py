70a2e35dbc1da0031f088ba95fcd207299da06f3
Sreejith <sreejithcs895@gmail.com>
2026-01-10 15:36:25 +0530

    debug false

--- furniture/furniture/settings.py
+++ furniture/furniture/settings.py
@@ -19,10 +19,10 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    '23.21.243.54'
+    'modulefurnitures.com', 'www.modulefurnitures.com'
 ]
 
 
