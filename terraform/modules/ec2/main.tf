3ea62eaf86479e7f56223c4f460c35bf8a91694a
Sreejith <sreejithcs895@gmail.com>
2026-01-11 20:09:28 +0530

    storage increased

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -10,6 +10,12 @@ resource "aws_instance" "module_host" {
         Name = "module-prod"
     }
 
+    root_block_device {
+        volume_size = 20      
+        volume_type = "gp3"   
+        delete_on_termination = true
+    }
+
     user_data = <<-EOF
             #!/bin/bash
             apt update -y
